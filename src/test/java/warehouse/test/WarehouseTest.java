01334227cd962d5e249552bb2660e143da55f889
Javier Rios Selent <rsjavier14>
2020-06-10 11:52:04 -0400

    Added empty Warehouse and WarehouseTest classes

---
+++ src/test/java/warehouse/test/WarehouseTest.java
@@ -0,0 +1,14 @@
+package warehouse.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WarehouseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
