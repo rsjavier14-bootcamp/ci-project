e87fab651b29bb42fcdc0b2ec68e6be613718f6a
Javier Rios Selent <rsjavier14>
2020-06-10 14:14:01 -0400

    Added methods for the Warehouse class

--- src/main/java/warehouse/Warehouse.java
+++ src/main/java/warehouse/Warehouse.java
@@ -2,4 +2,23 @@ package warehouse;
 
 public class Warehouse {
 
+	private String _name;
+
+	public Warehouse(String name) {
+		assert name != null;
+
+		if (name.trim().length() < 5 || name.trim().length() > 15)
+			throw new IllegalArgumentException("Debe ingresar un minimo de 5 caracteres y un maximo de 15 caracteres");
+
+		_name = name.trim();
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
 }
